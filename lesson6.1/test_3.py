792ad9a0ed7d558eb8f5083aeddcc5515f9815f2
Viktoriabel98 <belousova.v98@yahoo.com>
2024-08-08 23:48:07 +0300

    fix2

--- lesson6.1/test_3.py
+++ lesson6.1/test_3.py
@@ -13,10 +13,14 @@ def test_shop_form(chrome_browser):
     chrome_browser.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
     chrome_browser.find_element(By.ID, "shopping_cart_container").click()
     chrome_browser.find_element(By.ID, "checkout").click()
-    chrome_browser.find_element(By.ID, "first-name").send_keys("Evgen")
-    chrome_browser.find_element(By.ID, "last-name").send_keys("Voronov")
+    chrome_browser.find_element(By.ID, "first-name").send_keys("Viktoria")
+    chrome_browser.find_element(By.ID, "last-name").send_keys("Vasyura")
     chrome_browser.find_element(By.ID, "postal-code").send_keys("601500")
     chrome_browser.find_element(By.ID, "continue").click()
     total_price = chrome_browser.find_element(By.CLASS_NAME, 'summary_total_label')
     total = total_price.text.strp().replace("Total: $", "")
+
+    expected_total = "58.29"
+    assert total == expected_total
+    print(f"Итоговая сумма равна {total}")
 
