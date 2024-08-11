f61f1f7796c585cddf3ddde18702b69af38b4ad9
Viktoriabel98 <belousova.v98@yahoo.com>
2024-08-11 17:49:36 +0300

    draft

---
+++ Lesson7/Data_types/Pages/datafildes.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+class DataFild:
+    def __init__(self, browser):
+        self.browser = browser
+
+def find_fields(self):
---
+++ Lesson7/const.py
@@ -0,0 +1,3 @@
+Test_URL = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+Calculator_URL = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+Shop_URL = "https://www.saucedemo.com/"
