846ac2d522a00bd0694ee1cdcd738615496dee6c
Viktoriabel98 <belousova.v98@yahoo.com>
2024-08-26 22:53:24 +0300

    fix6

--- Lesson7/Calculator/test_2.py
+++ Lesson7/Calculator/test_2.py
@@ -4,6 +4,6 @@ def test_calculator_assert(chrome_browser):
        calcmain = CalcMain(chrome_browser)
        calcmain.insert_time()
        calcmain.clicking_buttons()
-
+       calcmain.wait_button_gettext()
        assert "15" in calcmain.wait_button_gettext()
        
--- Lesson7/Data_types/Pages/mainpage.py
+++ Lesson7/Data_types/Pages/mainpage.py
@@ -9,33 +9,20 @@ class MainPage:
         self.browser = browser
         self.browser.get(Test_URL)
 
-    def find_fields(self):
-        self._first_name = (By.NAME, "first-name")
-        self._last_name = (By.NAME, "last-name")
-        self._address = (By.NAME, "address")
-        self._email = (By.NAME, "e-mail")
-        self._phone = (By.NAME, "phone")
-        self._zip_code = (By.NAME, "zip-code")
-        self._city = (By.NAME, "city")
-        self._country = (By.NAME, "country")
-        self._job_position = (By.NAME, "job-position")
-        self._company = (By.NAME, "company")
-        self._button = (By.TAG_NAME, "button")
-
     def filling_in_the_fields(self):
         self.browser.find_element(By.NAME, "first-name").send_keys(first_name)
         self.browser.find_element(By.NAME, "last-name").send_keys(last_name)
         self.browser.find_element(By.NAME, "address").send_keys(address)
-        self.browser.find_element(By.NAME, "email").send_keys(email)
+        self.browser.find_element(By.NAME, "e-mail").send_keys(email)
         self.browser.find_element(By.NAME, "phone").send_keys(phone)
         self.browser.find_element(By.NAME, "zip-code").send_keys(zip_code)
         self.browser.find_element(By.NAME, "city").send_keys(city)
         self.browser.find_element(By.NAME, "country").send_keys(country)
-        self.browser.find_element(By.NAME, "job_position").send_keys(job_position)
+        self.browser.find_element(By.NAME, "job-position").send_keys(job_position)
         self.browser.find_element(By.NAME, "company").send_keys(company)
 
     def click_submit_button(self):
-        WebDriverWait(self.browser, 40, 0.1).until(EC.element_to_be_clickable(self._button)).click()    
+        WebDriverWait(self.browser, 40, 0.1).until(EC.element_to_be_clickable((By.TAG_NAME, "button"))).click()    
     
 
 
