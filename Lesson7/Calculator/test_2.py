01010eb1f37322c1b940b535dca02961f2c81781
Viktoriabel98 <belousova.v98@yahoo.com>
2024-09-08 22:45:08 +0300

    homework8fix1

--- Lesson7/Calculator/test_2.py
+++
@@ -1,9 +0,0 @@
-from Lesson7.Calculator.Calcmainpage import CalcMain
-
-def test_calculator_assert(chrome_browser):
-       calcmain = CalcMain(chrome_browser)
-       calcmain.insert_time()
-       calcmain.clicking_buttons()
-
-       assert "15" in calcmain.wait_button_gettext()
-       
